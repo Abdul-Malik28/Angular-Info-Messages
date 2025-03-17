d1421cfbcc15dd195ea9c0b5bee7063e3911bd15
Abdul_Malik <malik.info28@gmail.com>
2025-03-17 20:15:28 +0530

    Usinf OnPush Strategy

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, NgZone, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -8,6 +8,7 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CounterComponent implements OnInit {
   private zone = inject(NgZone);
@@ -26,7 +27,7 @@ export class CounterComponent implements OnInit {
 
     // setTimeout(() => {
     //   console.log('Timer expired!');
-    // }, 5000);
+    // }, 5000);  
 
     // ---Avoiding Zone Pollution--- //
     this.zone.runOutsideAngular(() => {

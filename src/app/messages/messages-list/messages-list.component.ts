8f1c87380d951658a392013ff6186f060e55376e
Abdul_Malik <malik.info28@gmail.com>
2025-03-17 23:22:25 +0530

    added async pipe

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,39 +1,43 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
   // messages = input.required<string[]>();
   private messagesService = inject(MessagesService);
 
-  private cdRef = inject(ChangeDetectorRef);
+  // private cdRef = inject(ChangeDetectorRef);
 
-  private destroyRef = inject(DestroyRef);
+  // private destroyRef = inject(DestroyRef);
 
-  // messages = this.messagesService.allMessages;
-  // get messages() {
-  //   return this.messagesService.allMessages;
+  // // messages = this.messagesService.allMessages;
+  // // get messages() {
+  // //   return this.messagesService.allMessages;
+  // // }
+
+  // messages: string[] = [];
+
+  // ngOnInit() {
+  //   const subscribtion = this.messagesService.messages$.subscribe((messages) => {
+  //     this.messages = messages;
+  //     this.cdRef.markForCheck();
+  //   });
+
+  //   this.destroyRef.onDestroy(() => {
+  //     subscribtion.unsubscribe();
+  //   });
   // }
 
-  messages: string[] = [];
-
-  ngOnInit() {
-    const subscribtion = this.messagesService.messages$.subscribe((messages) => {
-      this.messages = messages;
-      this.cdRef.markForCheck();
-    });
-
-    this.destroyRef.onDestroy(() => {
-      subscribtion.unsubscribe();
-    });
-  }
+  messages$ = this.messagesService.messages$;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
